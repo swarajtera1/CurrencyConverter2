d9015cc8c42a4442dff35df71752495c6a73472d
Swaraj <tswarajreddy@gmail.com>
2021-10-16 09:36:35 -0500

    Currency Conversion test cases

--- CurrencyConverterTests/UnitTest1.cs
+++ CurrencyConverterTests/UnitTest1.cs
@@ -1,4 +1,12 @@
 using NUnit.Framework;
+using CurrencyConverter;
+using CurrencyConverter.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using CurrencyServices.Implementation;
 
 namespace CurrencyConverterTests
 {
@@ -10,9 +18,20 @@ namespace CurrencyConverterTests
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            var mockRepo = new Mock<CurrencyService>();
+            CurrencyConverterController home = new CurrencyConverterController(mockRepo.Object);
+            JsonResult result = (JsonResult)await home.ConvertAmountCurrencies("USD", "PKR", 10);
+
+
+            var json = JsonConvert.SerializeObject(result.Value);
+
+            var deserializeData = JsonConvert.DeserializeObject<ResponseObject>(json);
+
+            Assert.AreEqual(1690.711, deserializeData.Data);
+
+
         }
     }
 }
